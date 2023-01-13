07f3ae58e4c6b3452110c65e5b854f829f62c1cc
fendyifgl <111411914+fendyifgl@users.noreply.github.com>
2023-01-13 21:03:13 +0700

    Add files via upload

---
+++ credential.php
@@ -0,0 +1,61 @@
+<?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    
+    include_once 'asset/aws/aws-autoloader.php';
+
+    use Aws\SecretsManager\SecretsManagerClient; 
+    use Aws\Exception\AwsException;
+
+    $client = new SecretsManagerClient([
+        'version' => 'latest',
+        'region' => 'ap-southeast-3'
+    ]);
+
+    //$secretName = 'nprd/dev/jg2mq/hlindo';
+    $secretName = 'arn:aws:secretsmanager:ap-southeast-3:402663288391:secret:nprd/dev/jg2mq/hlindo-t5UJRr';
+
+    try {
+        $result = $client->getSecretValue([
+            'SecretId' => $secretName,
+        ]);
+    } catch (AwsException $e) {
+        $error = $e->getAwsErrorCode();
+        if ($error == 'DecryptionFailureException') {
+            // Secrets Manager can't decrypt the protected secret text using the provided AWS KMS key.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        else if ($error == 'InternalServiceErrorException') {
+            // An error occurred on the server side.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        else if ($error == 'InvalidParameterException') {
+            // You provided an invalid value for a parameter.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        else if ($error == 'InvalidRequestException') {
+            // You provided a parameter value that is not valid for the current state of the resource.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        else if ($error == 'ResourceNotFoundException') {
+            // We can't find the resource that you asked for.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        else {
+            throw $e;
+        }
+    }
+
+    if (isset($result['SecretString'])) {
+        $awssecretmanager = json_decode($result['SecretString'], true);
+    } else {
+        $awssecretmanager = json_decode(base64_decode($result['SecretBinary']), true);
+    }
+
+    $config['db_username'] = $awssecretmanager['username'];
+    $config['db_password'] = $awssecretmanager['password'];
+?>
---
+++ secret_helper.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+    $ci = &get_instance();
+    $ci->load->config('credential');
+
+    function get_db_username() {
+        $ci = &get_instance();
+
+        return $ci->config->item('db_username');
+    }
+
+    function get_db_password() {
+        $ci = &get_instance();
+
+        return $ci->config->item('db_password');
+    }
+
+    function get_ftp_hostname() {
+        return "storage-arsip.ifg-life.id";
+    }
