8ab88e7d5d41a352cd958875bc5408b4f3389e8b
fendyifgl <111411914+fendyifgl@users.noreply.github.com>
2023-01-13 21:50:49 +0700

    Add files via upload

---
+++ secret.php
@@ -0,0 +1,128 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+    require 'aws-autoloader.php';
+
+    use Aws\SecretsManager\SecretsManagerClient; 
+    use Aws\Exception\AwsException;
+
+    $client = new SecretsManagerClient([
+        'version' => 'latest',
+        'region' => 'ap-southeast-3'
+    ]);
+
+    if (@$_POST['sn']) {
+        $secretName = $_POST['sn'];
+    } else {
+        $secretName = 'arn:aws:secretsmanager:ap-southeast-3:402663288391:secret:prd/legacy/glindo-SYulQt';
+    }
+
+    try {
+        $result = $client->getSecretValue([
+            'SecretId' => $secretName,
+        ]);
+    } catch (AwsException $e) {
+        $error = $e->getAwsErrorCode();
+        if ($error == 'DecryptionFailureException') {
+            // Secrets Manager can't decrypt the protected secret text using the provided AWS KMS key.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        else if ($error == 'InternalServiceErrorException') {
+            // An error occurred on the server side.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        else if ($error == 'InvalidParameterException') {
+            // You provided an invalid value for a parameter.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        else if ($error == 'InvalidRequestException') {
+            // You provided a parameter value that is not valid for the current state of the resource.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        else if ($error == 'ResourceNotFoundException') {
+            // We can't find the resource that you asked for.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        else {
+            throw $e;
+        }
+    }
+
+    if (isset($result['SecretString'])) {
+        $secretDB = $result['SecretString'];
+    } else {
+        $secretDB = base64_decode($result['SecretBinary']);
+    }
+
+    echo "DB : $secretDB";
+
+
+
+
+
+
+
+
+
+
+    $clientFTP = new SecretsManagerClient([
+        'version' => 'latest',
+        'region' => 'ap-southeast-3'
+    ]);
+
+    if (@$_POST['sn']) {
+        $secretName = $_POST['sn'];
+    } else {
+        $secretName = 'arn:aws:secretsmanager:ap-southeast-3:402663288391:secret:prd/legacy/ftpglindo-KQMeE0';
+    }
+
+    try {
+        $result = $clientFTP->getSecretValue([
+            'SecretId' => $secretName,
+        ]);
+    } catch (AwsException $e) {
+        $error = $e->getAwsErrorCode();
+        if ($error == 'DecryptionFailureException') {
+            // Secrets Manager can't decrypt the protected secret text using the provided AWS KMS key.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        else if ($error == 'InternalServiceErrorException') {
+            // An error occurred on the server side.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        else if ($error == 'InvalidParameterException') {
+            // You provided an invalid value for a parameter.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        else if ($error == 'InvalidRequestException') {
+            // You provided a parameter value that is not valid for the current state of the resource.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        else if ($error == 'ResourceNotFoundException') {
+            // We can't find the resource that you asked for.
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+        else {
+            throw $e;
+        }
+    }
+
+    if (isset($result['SecretString'])) {
+        $secretFTP = $result['SecretString'];
+    } else {
+        $secretFTP = base64_decode($result['SecretBinary']);
+    }
+
+    echo "FTP : $secretFTP";
+?>
