58c43815ff64a97fd489fc638a487d3a0b2a39c4
fendyifgl <111411914+fendyifgl@users.noreply.github.com>
2023-01-20 20:23:16 +0700

    Update secretmanager.php

--- secretmanager.php
+++ secretmanager.php
@@ -2,7 +2,7 @@
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 
-    require 'assets/aws/aws-autoloader.php';
+    require 'asset/aws/aws-autoloader.php';
 
     use Aws\SecretsManager\SecretsManagerClient; 
     use Aws\Exception\AwsException;
