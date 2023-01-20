e8dc05213bb08a00ed2f5031924a01dcaa1fd66d
fendyifgl <111411914+fendyifgl@users.noreply.github.com>
2023-01-20 20:55:31 +0700

    Update secret_helper.php

--- secret_helper.php
+++ secret_helper.php
@@ -13,19 +13,3 @@
 
         return $ci->config->item('db_password');
     }
-
-    function get_ftp_hostname() {
-        return "storage-arsip.ifg-life.id";
-    }
-
-    function get_ftp_username() {
-        $ci = &get_instance();
-
-        return $ci->config->item('ftp_username');
-    }
-
-    function get_ftp_password() {
-        $ci = &get_instance();
-
-        return $ci->config->item('ftp_password');
-    }
