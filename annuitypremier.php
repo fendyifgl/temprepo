59ee62159b0ff611b6881d0e91c84518de1d86a9
fendyifgl <111411914+fendyifgl@users.noreply.github.com>
2023-01-21 03:44:05 +0700

    Add files via upload

--- annuitypremier.php
+++ annuitypremier.php
@@ -1,221 +1,129 @@
-<script type="text/javascript" src="<?= base_url();?>assets/plugins/jquery-1.10.2.min.js"></script>
-<script type="text/javascript" src="<?= base_url();?>assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?= base_url();?>assets/plugins/loadingoverlay.min.js"></script>
-<script type="text/javascript">
-	function Popitup(url) {
-		newwindow=window.open(url,'name','height=850,width=1200, menubar=no, scrollbars=no, resizable=yes, copyhistory=no,');
-		if (window.focus) {newwindow.focus()}
-		return false;
-	}
+<?php
 
-	function CetakPDF(){
-		var restorepage = document.body.innerHTML;
-		var printcontent = document.getElementById('Testis').innerHTML;
-		document.body.innerHTML = printcontent;
-		window.print();
-		document.body.innerHTML = restorepage;
+class Annuitypremier extends CI_Controller {
+    
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('modsimulasi');
+		$this->load->model('modmaster');
+    }
+
+    function hasil() {
+		$buildid = $this->input->get('buildid');
+
+		$data['pos'] = $this->modsimulasi->get_hitung($buildid);
+		
+		$this->load->view('hasil/annuitypremier', $data);
 	}
 	
-	function Back(noid) {
-		$.LoadingOverlay("show");
+	function cetak() {
+        $buildid = $this->input->get('buildid');
 		
-		window.location.href = '<?=str_replace("simulasi/","",base_url("prospek/proposal?id="));?>'+noid;
+		$data['pos'] = $this->modsimulasi->get_hitung($buildid);
+		
+		$this->load->view('pdf/annuitypremier', $data);
 	}
-</script>
 
-<!-- BEGIN PAGE CONTENT-->
-<div class="" id="Testis">
-	<div class="row" align="center">
-		<div class="col-xs-12">
-			<div class="well">
-				<address>
-				Illustrasi ini hanya merupakan pendekatan/proyeksi dari jumlah dana yang diinvestasikan dan bukan merupakan bagian dari kontrak asuransi
-				<address>
-			</div>
-		</div>
-	 </div>
-	<div class="row invoice-logo">
-		<div class="col-xs-5" align="justify">
-			<p>
-				 <h4>PT. ASURANSI JIWA IFG LIFE</h4>
-			</p>
-		</div>
-		Graha CIMB Niaga Lt. 6, Jl. Jend. Sudirman Kav 58 Jakarta - 12190
-		<div class="col-xs-7 invoice-logo-space" align="right">
-			<img src="<?php echo base_url(); ?>/assets/img/logo-js.png" alt=""/>
-		</div>
-	</div>
-	&nbsp;
-	<div class="row" align="center">
-		<div class="col-xs-12">
-			<input id="buildid" name="buildid" value="<?=$_GET['buildid'] ?>" style="display: none"></input>
-			<div class="well" style="font-weight:bold;font-size:16px">
-				<?=strtoupper(strtolower($pos['NAMAPRODUK']))?>
-			</div>
-		</div>
-	 </div>
-	<hr/>
-	<div class="row">
-		<div class="col-xs-12">
-			<div class="well" style="font-weight:bold;font-size:18px">
-				CALON PEMEGANG POLIS
-			</div>
-			<div class="table-responsive">
-				<table class="table table-striped table-hover" width="100%">			
-					<tbody>
-						<tr>
-							<td align="justify" style="font-size:18px" width="20%">Nama Pemegang Polis </td>
-							<td align="center" style="font-size:18px" width="10px">:</td>
-							<td align="left" style="font-size:18px"><?=$pos['NAMAPEMPOL'];?></td>						
-						</tr>
-						<tr>
-							<td align="justify" style="font-size:18px">Tanggal Lahir </td>
-							<td align="center" style="font-size:18px">:</td>
-							<td align="left" style="font-size:18px"><?=$pos['TGLLAHIRPEMPOL'];?></td>
-						</tr>
-						<tr>
-							<td align="justify" style="font-size:18px">Jenis Kelamin </td>
-							<td align="center" style="font-size:18px">:</td>
-							<td align="left" style="font-size:18px"><?=$pos['JENISKELAMINPEMPOL'];?></td>						
-						</tr>
-						<tr>
-							<td align="justify" style="font-size:18px">Pekerjaan </td>
-							<td align="center" style="font-size:18px">:</td>
-							<td align="left" style="font-size:18px"><?=$pos['PEKERJAANPEMPOL'];?></td>						
-						</tr>
-						<tr>
-							<td align="justify" style="font-size:18px">Hobi </td>
-							<td align="center" style="font-size:18px">:</td>
-							<td align="left" style="font-size:18px"><?=$pos['HOBIPEMPOL'];?></td>						
-						</tr>
-					<tbody>
-				</table>
-			</div>						
-		</div>
-	</div>
-	<hr>
-	<div class="row">
-		<div class="col-xs-12">
-			<div class="well" style="font-weight:bold;font-size:18px">
-				CALON TERTANGGUNG
-			</div>
-			<div class="table-responsive">
-				<table class="table table-striped table-hover" width="100%">			
-					<tbody>
-						<tr>
-							<td align="justify" style="font-size:18px" width="20%">Nama Tertanggung </td>
-							<td align="center" style="font-size:18px" width="10px">:</td>
-							<td align="left" style="font-size:18px"><?=$pos['NAMATERTANGGUNG'];?></td>						
-						</tr>
-						<tr>
-							<td align="justify" style="font-size:18px">Tanggal Lahir </td>
-							<td align="center" style="font-size:18px">:</td>
-							<td align="left" style="font-size:18px"><?=$pos['TGLLAHIRTERTANGGUNG'];?></td>						
-						</tr>
-						<tr>
-							<td align="justify" style="font-size:18px">Jenis Kelamin </td>
-							<td align="center" style="font-size:18px">:</td>
-							<td align="left" style="font-size:18px"><?=$pos['JENISKELAMINTERTANGGUNG'];?></td>						
-						</tr>
-						<tr>
-							<td align="justify" style="font-size:18px">Pekerjaan </td>
-							<td align="center" style="font-size:18px">:</td>
-							<td align="left" style="font-size:18px"><?=$pos['PEKERJAANTERTANGGUNG'];?></td>						
-						</tr>
-						<tr>
-							<td align="justify" style="font-size:18px">Hobi </td>
-							<td align="center" style="font-size:18px">:</td>
-							<td align="left" style="font-size:18px"><?=$pos['HOBITERTANGGUNG'];?></td>						
-						</tr>
-						<tr>
-							<td align="justify" style="font-size:18px">Status </td>
-							<td align="center" style="font-size:18px">:</td>
-							<td align="left" style="font-size:18px">
-								<?php switch($pos['MERITALSTATUSTERTANGGUNG']) {
-									case 'L':
-										echo "Lajang";
-										break;
-									case 'K':
-										echo "Kawin";
-										break;
-									case 'J':
-										echo "Janda";
-										break;
-									case 'D':
-										echo "Duda";
-										break;
-								} ?>
-							</td>						
-						</tr>
-					<tbody>
-				</table>
-			</div>					
-		</div>
-	</div>			
-	<hr>
-	<div class="row">
-		<div class="col-xs-12">
-			<div class="well" style="font-weight:bold;font-size:18px">
-				DATA PERTANGGUNGAN
-			</div>
-			<div class="table-responsive">
-				<table class="table table-striped table-hover" width="100%">			
-					<tbody>
-						<tr>
-							<td align="justify" style="font-size:18px" width="20%">Cara Bayar </td>
-							<td align="center" style="font-size:18px" width="10px">:</td>
-							<td align="left" style="font-size:18px"><?=$pos['NAMACARABAYAR'];?></td>						
-						</tr>
-						<tr>
-							<td align="justify" style="font-size:18px">Manfaat Hari Tua </td>
-							<td align="center" style="font-size:18px">:</td>
-							<td align="left" style="font-size:18px"><?= number_format($pos['JPHT'],0,',','.');?></td>						
-						</tr>
-						<?php if (in_array($pos['MERITALSTATUSTERTANGGUNG'], array('K','J','D'))) { ?>
-						<tr>
-							<td align="justify" style="font-size:18px">Manfaat Janda/Duda </td>
-							<td align="center" style="font-size:18px">:</td>
-							<td align="left" style="font-size:18px"><?= number_format($pos['JPJD'],0,',','.');?></td>		
-						</tr>
-						<tr>
-							<td align="justify" style="font-size:18px">Manfaat Yatim </td>
-							<td align="center" style="font-size:18px">:</td>
-							<td align="left" style="font-size:18px"><?= number_format($pos['JPYT'],0,',','.');?></td>		
-						</tr>
-						<?php } ?>
-						<tr>
-							<td align="justify" style="font-size:18px">Premi </td>
-							<td align="center" style="font-size:18px">:</td>
-							<td align="left" style="font-size:18px"><?= number_format($pos['PREMI'],0,',','.');?></td>						
-						</tr>					
-					<tbody>
-				</table>
-			</div>					
-		</div>
-	</div>
-</div>
+    function save() {
+		$kdproduk = $this->input->post('kdproduk');
+        $noagen = $this->input->post('noagen');
+        $sama = $this->input->post('tertanggungsamadenganpemegangpolis');
+        $noidpp = $this->input->post('noktppemegangpolis');
+        $manfaattertanggung = $this->input->post('manfaattertanggung');
+        $buildid = $this->modmaster->gen_build_id();
+        $pempol = $this->modmaster->get_klien(array(array('operator' => 'WHERE', 'text' => 'noid', 'comparison' => '=', 'value' => "'$noidpp'")));
+        $produk = api_curl("/master/produk/$kdproduk", 'GET');
+        
+        // Tertanggung berbeda
+        if (!$sama) {
+            $noidtt = $this->input->post('noktpcalontertanggung');
+            $tertanggung = $this->modmaster->get_klien(array(array('operator' => 'WHERE', 'text' => 'noid', 'comparison' => '=', 'value' => "'$noidtt'")));
 
-<br><br><br><hr>
-			
-<div class="row">
-	<div class="col-md-12">
-		<span style="margin-left:2em">
-			<a href='javascript:void(0);' onClick="Popitup('<?=base_url("cetak?buildid=$pos[BUILDID]")?>')">
-			 Cetak PDF <i class="m-icon-swapright m-icon-white"></i>
-			</a>
-		</span>
-			
-		<br><br>
-		<span style="margin-left:2em">
-			<a href="javascript:void(0)" onclick="Back('<?=$pos['NOIDPEMPOL']?>');" />
-			Buat Proposal Kembali  <i class="m-icon-swapright m-icon-white"></i>
-			</a>
-		</span>
-	</div>
-</div>
+            $data['kdpekerjaan'] = $this->input->post('kdpekerjaancalontertanggung');
+            $data['kdhobi'] = $this->input->post('kdhobicalontertanggung');
+            $data['namaklien'] = $this->input->post('namacalontertanggung');
+            $data['kdjeniskelamin'] = $this->input->post('kdjeniskelamincalontertanggung');
+            $data['tgllahir'] = "~TO_DATE('".$this->input->post('tanggallahircalontertanggung')."', 'dd-mm-yyyy')";
+            $data['noid'] = $noidtt;
+            $data['meritalstatus'] = $this->input->post('meritalstatustertanggung');
+            
+            // Tertanggung baru
+            if (!$tertanggung) {
+                $noctt = $this->modmaster->gen_no_klien();
+                $data['noklien'] = $noctt;
+                
+                $this->modmaster->insert('jaim_302_klien', $data);
+            } else {
+                $noctt = $tertanggung['NOKLIEN'];
 
-</div>
-<!-- END PAGE CONTENT-->
-</div>
-</div>
-<!-- END CONTENT -->
+                $this->modmaster->update('jaim_302_klien', $data, 'noklien', $noctt);
+            }
+
+            // Hitung Klien Tertanggung
+            $datat['buildid'] = $buildid;
+            $datat['noklien'] = $noctt;
+            $datat['kdpekerjaan'] = $data['kdpekerjaan'];
+            $datat['kdhobi'] = $data['namaklien'];
+            $datat['namaklien'] = $data['namaklien'];
+            $datat['kdjeniskelamin'] = $data['kdjeniskelamin'];
+            $datat['tgllahir'] = $data['tgllahir'];
+            $datat['noid'] = $noidtt;
+            $datat['meritalstatus'] = $data['meritalstatus'];
+            $datat['flag'] = 0;
+            $datat['tglrekam'] = "~sysdate";
+            $datat['userrekam'] = $noagen;
+        }
+
+        // Hitung Klien Pempol
+        $datap['buildid'] = $buildid;
+        $datap['noklien'] = $pempol['NOKLIEN'];
+        $datap['kdpekerjaan'] = $this->input->post('kdpekerjaanpemegangpolis');
+        $datap['kdhobi'] = $this->input->post('kdhobipemegangpolis');
+        $datap['namaklien'] = $this->input->post('namapemegangpolis');
+        $datap['kdjeniskelamin'] = $this->input->post('kdjeniskelaminpemegangpolis');
+        $datap['tgllahir'] = "~TO_DATE('".$this->input->post('tanggallahirpemegangpolis')."', 'dd-mm-yyyy')";
+        $datap['noid'] = $this->input->post('noktppemegangpolis');
+        $datap['meritalstatus'] = $this->input->post('meritalstatuspemegangpolis');
+        $datap['kdprovinsi'] = $this->input->post('kdprovinsipemegangpolis');
+        $datap['kdkotamadya'] = $this->input->post('kdkotamadyapemegangpolis');
+        $datap['kdkelurahan'] = $pempol['KDKELURAHAN'];
+        $datap['alamat'] = $this->input->post('alamatpemegangpolis');
+        $datap['email'] = $this->input->post('emailpemegangpolis');
+        $datap['telepon'] = $this->input->post('phonepemegangpolis');
+        $datap['hp'] = $this->input->post('handphonepemegangpolis');
+        $datap['flag'] = 0;
+        $datap['tglrekam'] = "~sysdate";
+        $datap['userrekam'] = $noagen;
+        $this->modmaster->insert('jaim_302_hitung_klien', $datap);
+        $this->modmaster->insert('jaim_302_hitung_klien', $sama ? $datap : $datat); 
+        
+        // Hitung
+        $datah['buildid'] = $buildid;
+        $datah['nocpp'] = $datap['noklien'];
+        $datah['noctt'] = $sama ? $datap['noklien'] : $noctt;
+        $datah['noagen'] = $noagen;
+        $datah['kdproduk'] = $kdproduk;
+        $datah['kdcarabayar'] = $this->input->post('kdcarabayar');
+        $datah['premi'] = $this->input->post('totalpremi');
+        $datah['premiberkala'] = 0;
+        $datah['topupsekaligus'] = 0;
+        $datah['topupberkala'] = 0;
+        $datah['periodetopup'] = 0;
+        $datah['jua'] = $this->input->post('manfaatharitua');
+        $datah['juamaksimal'] = !$produk['error'] ? $produk['message']['UAMAX'] : 0;
+        $datah['penghasilan'] = $this->input->post('penghasilansatutahun');
+        $datah['usiaproduktif'] = 0;
+        $datah['resikoawal'] = 0;
+        $datah['sisaresiko'] = 0;
+        $datah['totalresiko'] = 0;
+        $datah['jpht'] = $this->input->post('manfaatharitua');
+        $datah['jpjdyt'] = $this->input->post('meritalstatuspemegangpolis') == 'K' ? $this->input->post('manfaatjandaduda') : 0;
+        $datah['jpjd'] = $this->input->post('manfaatjandaduda');
+        $datah['jpyt'] = $this->input->post('manfaatyatim');
+        $datah['flag'] = '0';
+        $datah['tglrekam'] = '~sysdate';
+        $this->modmaster->update('jaim_302_hitung', $datah, 'buildid', $buildid, true);
+        
+		redirect(base_url("annuitypremier/hasil?buildid=$buildid"));
+	}
+}
