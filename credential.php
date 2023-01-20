5f62c379cf24ae879debcd48b3e0f57cf28b1a13
fendyifgl <111411914+fendyifgl@users.noreply.github.com>
2023-01-20 20:13:44 +0700

    Update credential.php

--- credential.php
+++ credential.php
@@ -11,48 +11,11 @@
         'region' => 'ap-southeast-3'
     ]);
 
-    $secretNameDB = 'arn:aws:secretsmanager:ap-southeast-3:402663288391:secret:prd/legacy/enota-NS12tP';
-    $secretNameFTP = 'arn:aws:secretsmanager:ap-southeast-3:402663288391:secret:prd/legacy/ftpenota-MK8goO';
+    $secretNameDB = 'arn:aws:secretsmanager:ap-southeast-3:402663288391:secret:prd/legacy/aims-wSbb0V';
 
     try {
         $resultDB = $client->getSecretValue([
             'SecretId' => $secretNameDB,
-        ]);
-    } catch (AwsException $e) {
-        $error = $e->getAwsErrorCode();
-        if ($error == 'DecryptionFailureException') {
-            // Secrets Manager can't decrypt the protected secret text using the provided AWS KMS key.
-            // Handle the exception here, and/or rethrow as needed.
-            throw $e;
-        }
-        else if ($error == 'InternalServiceErrorException') {
-            // An error occurred on the server side.
-            // Handle the exception here, and/or rethrow as needed.
-            throw $e;
-        }
-        else if ($error == 'InvalidParameterException') {
-            // You provided an invalid value for a parameter.
-            // Handle the exception here, and/or rethrow as needed.
-            throw $e;
-        }
-        else if ($error == 'InvalidRequestException') {
-            // You provided a parameter value that is not valid for the current state of the resource.
-            // Handle the exception here, and/or rethrow as needed.
-            throw $e;
-        }
-        else if ($error == 'ResourceNotFoundException') {
-            // We can't find the resource that you asked for.
-            // Handle the exception here, and/or rethrow as needed.
-            throw $e;
-        }
-        else {
-            throw $e;
-        }
-    }
-
-    try {
-        $resultFTP = $client->getSecretValue([
-            'SecretId' => $secretNameFTP,
         ]);
     } catch (AwsException $e) {
         $error = $e->getAwsErrorCode();
@@ -92,14 +55,6 @@
         $awssecretmanagerdb = json_decode(base64_decode($resultDB['SecretBinary']), true);
     }
 
-    if (isset($resultFTP['SecretString'])) {
-        $awssecretmanagerftp = json_decode($resultFTP['SecretString'], true);
-    } else {
-        $awssecretmanagerftp = json_decode(base64_decode($resultFTP['SecretBinary']), true);
-    }
-
     $config['db_username'] = $awssecretmanagerdb['username'];
     $config['db_password'] = $awssecretmanagerdb['password'];
-    $config['ftp_username'] = $awssecretmanagerftp['username'];
-    $config['ftp_password'] = $awssecretmanagerftp['password'];
 ?>
